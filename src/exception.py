5536133171816cce9c5818ce16f768ed45a3b43f
Anuj Jadhav <anujpjadhav5@gmail.com>
2024-09-05 07:31:42 +0530

    Dataset added - notebook

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-from src.logger import logging
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -19,6 +18,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-
-
